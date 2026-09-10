b2599e17c65388793db89c765dc9ec86a4de1624
Julio Santiago Villaseca <juliosantiagovillaseca@gmail.com>
2026-09-10 08:59:03 -0600

    Agregar js/03.js

---
+++ js/03.js
@@ -0,0 +1,29 @@
+// Variables con const
+
+const pais = 'Costa Rica';
+console.log(pais);
+
+const pais2 = 'Mexico';
+console.log(pais2);
+
+const pais3 = 'Estados Unidos';
+console.log(pais3); 
+
+const pais4 = 'España';
+console.log(pais4);
+
+let disponible;
+let paisModificable = false;
+
+disponible = true;
+
+const ciudad = 'Coatzacoalcos', encendido = true, edad = 20;
+const temperatura = 25; 
+
+console.log(pais);
+
+const nombre_producto = 'Monitor LCD';
+const nombreProducto = 'Monitor LCD'; 
+const NombreProducto = 'Monitor LCD';  
+
+console.log(temperatura);
