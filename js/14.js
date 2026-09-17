3920aa42e2572385d8a940518080d8eb7ac85c2c
Julio Santiago Villaseca <juliosantiagovillaseca@gmail.com>
2026-09-17 08:00:38 -0600

    Practica Hoy

---
+++ js/14.js
@@ -0,0 +1,17 @@
+//Arreglos o Arrays
+
+const numeros = [10, 20, 3, 100 , 81];
+
+console.table(numeros);
+
+const tortugasNinja = new Array('Donatelo', 'Migueo Angel', 'Leonardo', 'Rafael');
+
+console.table(tortugasNinja);
+
+const dragonBall = new Array('Goku', 'Gohan', 'Vegueta', 'Bulma' , 'Piccolo', 'Freezer', 'Bills' ,'Whiss');
+
+console.table(dragonBall);
+
+const jujutsuKaisen = ['Gojo', 'Megumi', 'Nobara', 'Sukuna'];
+
+console.table(jujutsuKaisen);
