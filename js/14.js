886f11078c13c6a75e181652463c0ad3a4e567b4
Julio Santiago Villaseca <juliosantiagovillaseca@gmail.com>
2026-09-17 08:21:03 -0600

    14

--- js/14.js
+++ js/14.js
@@ -15,3 +15,25 @@ console.table(dragonBall);
 const jujutsuKaisen = ['Gojo', 'Megumi', 'Nobara', 'Sukuna'];
 
 console.table(jujutsuKaisen);
+
+const narutoShipuden = new Array('Itachi', 'Sasuke', 'Minato', 'Obito', 'Madara' );
+console.table(narutoShipuden);
+
+const arrVariado = ['Mesa', 15, false , 'si' , null, {nombre: 'Juan', profesion: 'Programador'}];
+
+console.table(arrVariado);
+
+
+//Acceder alos valores de un arreglo 
+console.log(arrVariado[5]);
+console.log(arrVariado[200]);//Cuando tratamos de imprimir un indice que no existe "IMPRIME undefined"
+
+//Conocer la extension del arreglo
+console.log(numeros.length);
+console.log(tortugasNinja.length);
+console.log(arrVariado.length);
+
+//Imprimir elementos con forEach
+tortugasNinja.forEach( function(nombre){
+    console.log(nombre);
+})
