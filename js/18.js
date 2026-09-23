ca9327227eeeb5839dc6a5da6c1352901f489fc8
Julio Santiago Villaseca <juliosantiagovillaseca@gmail.com>
2026-09-23 07:39:12 -0600

    Practica Hoy

---
+++ js/18.js
@@ -0,0 +1,10 @@
+//Funciones con parametros y argumentos
+
+function sumar(num1, num2 = 5) {  //Parametros
+    console.log(num1+num2);
+}
+
+sumar(10, 10);//argumentos o valores reales
+sumar(100, 10);
+sumar(100, 1000);
+sumar(60, 1);
